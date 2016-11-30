a2def67886d76f0b5d1f83c83c3cec2f7ce436e2
Cyle Carlson <jacerider@gmail.com>
2016-11-30 10:51:06 -0600

    Support for overrides of title and icon.

--- micon_link/src/Plugin/Field/FieldFormatter/MiconLinkFormatter.php
+++ micon_link/src/Plugin/Field/FieldFormatter/MiconLinkFormatter.php
@@ -5,7 +5,13 @@ namespace Drupal\micon_link\Plugin\Field\FieldFormatter;
 use Drupal\link\Plugin\Field\FieldFormatter\LinkFormatter;
 use Drupal\Component\Utility\Unicode;
 use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\micon\MiconIconizeTrait;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Path\PathValidatorInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Field\FieldDefinitionInterface;
+use Drupal\Core\Utility\Token;
 
 /**
  * Plugin implementation of the 'micon_link' formatter.
@@ -22,13 +28,100 @@ class MiconLinkFormatter extends LinkFormatter {
   use MiconIconizeTrait;
 
   /**
+   * The token service.
+   *
+   * @var \Drupal\Core\Utility\Token
+   */
+  protected $token;
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, $label, $view_mode, array $third_party_settings, PathValidatorInterface $path_validator, Token $token) {
+    parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings, $path_validator);
+    $this->token = $token;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $plugin_id,
+      $plugin_definition,
+      $configuration['field_definition'],
+      $configuration['settings'],
+      $configuration['label'],
+      $configuration['view_mode'],
+      $configuration['third_party_settings'],
+      $container->get('path.validator'),
+      $container->get('token')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return [
+      'title' => '',
+      'icon' => '',
+    ] + parent::defaultSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $summary = parent::settingsSummary();
+    if ($title = $this->getSetting('title')) {
+      $summary[] = t('Link title as @title', array('@title' => $title));
+    }
+    if ($icon = $this->getSetting('icon')) {
+      $summary[] = $this->micon('Icon as')->setIcon($icon)->setIconAfter();
+    }
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    $form = parent::settingsForm($form, $form_state);
+    $form['title'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Link title'),
+      '#default_value' => $this->getSetting('title'),
+      '#description' => $this->t('Will be used as the link title even if one has been set on the field. Supports token replacement.'),
+    ];
+    $form['icon'] = [
+      '#type' => 'micon',
+      '#title' => $this->t('Link icon'),
+      '#default_value' => $this->getSetting('icon'),
+      '#description' => $this->t('Will be used as the link icon even if one has been set on the field.'),
+    ];
+    return $form;
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function viewElements(FieldItemListInterface $items, $langcode) {
     $element = parent::viewElements($items, $langcode);
+    $entity = $items->getEntity();
+    $entity_type = $entity->getEntityTypeId();
+    $title = $this->getSetting('title');
+    $icon = $this->getSetting('icon');
     foreach ($element as $delta => &$item) {
-      if (!empty($item['#options']['attributes']['data-icon'])) {
-        $item['#title'] = $this->micon($item['#title'])->setIcon($item['#options']['attributes']['data-icon']);
+      if ($title) {
+        $item['#title'] = $this->token->replace($title, [$entity_type => $entity]);
+      }
+      if (!$icon && !empty($item['#options']['attributes']['data-icon'])) {
+        $icon = $item['#options']['attributes']['data-icon'];
+      }
+      if ($icon) {
+        $item['#title'] = $this->micon($item['#title'])->setIcon($icon);
         unset($item['#options']['attributes']['data-icon']);
       }
     }
