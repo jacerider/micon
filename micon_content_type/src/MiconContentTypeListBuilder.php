9c5b19dc3955bdc88efda9020aab842834322755
JaceRider <jacerider@gmail.com>
2017-12-06 11:36:01 -0600

    Show icons on list builder.

---
+++ micon_content_type/src/MiconContentTypeListBuilder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Drupal\micon_content_type;
+
+use Drupal\node\NodeTypeListBuilder;
+use Drupal\Core\Entity\EntityInterface;
+
+/**
+ * Provides a listing of ContentType.
+ */
+class MiconContentTypeListBuilder extends NodeTypeListBuilder {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildHeader() {
+    $header['icon'] = t('Icon');
+    return $header + parent::buildHeader();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildRow(EntityInterface $entity) {
+    $icon = micon_content_type_icon($entity);
+    if ($icon) {
+      $row['icon']['data']['#markup'] = micon()->setIcon($icon);
+    }
+    return $row + parent::buildRow($entity);
+  }
+
+}
